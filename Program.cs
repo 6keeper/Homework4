c2c2d2c856bc9b8e1bb29982a071298455b4cce1
6keeper <82912166+6keeper@users.noreply.github.com>
2021-08-01 23:49:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please insert number");
+            int Num = int.Parse(Console.ReadLine());
+            if (Num>=0 & Num<=30)
+            {
+                Console.WriteLine("Your number is between 0 and 30");
+            }
+            else if (Num>30 & Num<=60)
+            {
+                Console.WriteLine("Your number is between 31 and 60");
+            }
+            else if (Num>60 & Num <=100)
+            {
+                Console.WriteLine("Your number is between 60 and 100");
+            }
+            else
+            {
+                Console.WriteLine("Unknown number");
+            }
+        }
+    }
+}
