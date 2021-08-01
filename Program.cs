a0d4d9a49886540a5c4d6693c4a2784a38e58623
6keeper <82912166+6keeper@users.noreply.github.com>
2021-08-02 01:51:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = 200;
+            while (Num<500)
+            {
+                Num++;
+                if (Num%17 == 0)
+                {
+                    Console.Write($"{Num}, ");
+                }
+            }
+        }
+    }
+}
